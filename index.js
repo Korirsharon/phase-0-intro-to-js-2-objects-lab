95b13e1e913d64c13087fa2fc1224912c05ad772
Sharon Chepngeno <chepngenokorir19@gmail.com>
2025-03-09 14:34:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sharon Julia",
+    streetAddress: "550 Down St"
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  
+  console.log(updateEmployeeWithKeyAndValue(employee, "streetAddress", "501 Uptown St"));
+
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  
+  console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "300 Uhuru Highway"));
+
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  console.log(deleteFromEmployeeByKey(employee, "streetAddress"));
+  
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
+  console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress"));
+  
+  
+  
